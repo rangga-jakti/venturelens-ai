64e16bcb1b2c3ee52a50b875b22e6117f951c8f9
Rangga <ranggarvc@gmail.com>
2026-03-12 03:32:49 +0700

    fix railway deploy

--- venturelens_project/wsgi.py
+++ venturelens_project/wsgi.py
@@ -1,6 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'venturelens_project.settings.production')
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "venturelens_project.settings.production"
+)
 
 application = get_wsgi_application()
