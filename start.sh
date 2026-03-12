16d21adeaf91fb0e946be2b5ee8d4ea4191891c8
Rangga <ranggarvc@gmail.com>
2026-03-13 03:18:19 +0700

    Fix start.sh - remove set -e

--- start.sh
+++ start.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput --clear
 exec gunicorn venturelens_project.wsgi:application --bind 0.0.0.0:$PORT --workers 2
